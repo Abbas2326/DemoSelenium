f815636b72f0753a137f005012f5321cf367e610
mohaabba <mohaabba@lin76001635.corp.capgemini.com>
2024-03-21 11:01:16 +0530

    Myntra

--- src/main/java/com/scenarios/DemoScene5.java
+++ src/main/java/com/scenarios/DemoScene5.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.json.simple.parser.ParseException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -19,19 +20,16 @@ public class DemoScene5 {
 	static WebDriver driver;
 	static DriverUtils util;
 
-	@Test
-	public static void testStart ()throws IOException, ParseException {
-		jsonCredential();
-	}
-
 	@BeforeTest
 	private static void openApplication() {
-		util.OpenApp("https://www.myntra.com/");
+		driver=new ChromeDriver();
 		util = new DriverUtils(driver);
+		util.OpenApp("https://www.myntra.com/");
 	}
 
+	@Test
 	public static void jsonCredential() throws IOException, ParseException {
-		FileReader reader = new FileReader("./JSON/data.json");
+		FileReader reader = new FileReader("./JSON/scene5.json");
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, String> userData = mapper.readValue(reader, Map.class);
 		performSearch(userData);
@@ -51,7 +49,7 @@ public class DemoScene5 {
 	}
 
 	public static void firstProductPrice(Map<String, String> userData) {
-		String price = util.GetText(userData.get("product-price"));
+		String price = util.GetText(userData.get("first-product"));
 		int amount=Integer.parseInt(price.replaceAll("[^0-9]", ""));
 		System.out.println(amount);
 
@@ -60,12 +58,11 @@ public class DemoScene5 {
 		}else {
 			System.out.println("Price is too high!");
 		}
-
 	}
 
-	@AfterTest
+	@AfterMethod
 	public static void CloseApplication() {
-		driver.quit();
+		util.CloseApp();;
 	}
 
 }
